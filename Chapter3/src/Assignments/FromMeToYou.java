49ec718b5a8fc6614a0680b42b5e650c0f257669
46221501 <46221501@L815WD220023.edu.cbe.ab.ca>
2022-03-31 14:56:45 -0600

    2:56pm

---
+++ Chapter3/src/Assignments/FromMeToYou.java
@@ -0,0 +1,15 @@
+package Assignments;
+
+public class FromMeToYou 
+{
+
+	public static void main(String args[])
+	{
+		System.out.println("From: Mr. Wilson\r\n"
+				+ "Address: Acer Computer, Lab 314\r\n"
+				+ "Date: November 2, 2020\r\n"
+				+ "To: Enter Your First Name and Last Name\r\n"
+				+ "Message: Help! I'm trapped inside a computer!");
+	}
+}
+
