32048d45309416ebe4f37bd992d54e2acd82511f
46221501 <46221501@L815WD220023.edu.cbe.ab.ca>
2022-04-01 11:32:37 -0600

    11:32

---
+++ Chapter3/src/Assignments/BingoCard.java
@@ -0,0 +1,12 @@
+package Assignments;
+
+public class BingoCard 
+{
+
+	public static void main(String args[])
+	{
+		System.out.println(""
+				+ "B     I     N     G     O\r\n"
+				+ "6    24    36");
+	}
+}
