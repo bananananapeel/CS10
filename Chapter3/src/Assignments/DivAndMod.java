3979712c218a8702eb4bbc3db4e520374aa1cdf9
46221501 <46221501@L815WD220023.edu.cbe.ab.ca>
2022-04-14 14:38:22 -0600

    2:38pm

--- Chapter3/src/Assignments/DivAndMod.java
+++ Chapter3/src/Assignments/DivAndMod.java
@@ -1,10 +1,40 @@
 package Assignments;
 
+import java.util.Scanner;
+
 public class DivAndMod 
 {
+	/*
 
+	Program: ${DivAndMod}.java          Date: ${April 13 2022}
+
+
+	Author: Anjali Sanjay
+	School: CHHS
+	Course: Computer Science 10
+	 
+
+	*/
 	public static void main(String[] args) 
 	{
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter an Integer: ");
+		int x = input.nextInt();
+		
+		System.out.println("Enter a Second Integer: ");
+		int y = input.nextInt();
+		
+		int div = x / y;
+		int r = x % y;
+		int div2 = y / x;
+		int r2 = y % x;
+		
+		System.out.println(""
+				+ x +"/"+ y +"="+ div +"\r\n"
+				+ x +"%"+ y +"="+ r +"\r\n"
+				+ y +"/"+ x +"="+ div2 +"\r\n"
+				+ y +"%"+ x +"="+ r2 +"\r\n");
 		
 
 	}
