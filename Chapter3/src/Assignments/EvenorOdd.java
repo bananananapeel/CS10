be893170c5379324a276fd3187186b66eda1ba77
46221501 <46221501@L815WD220023.edu.cbe.ab.ca>
2022-04-22 11:07:14 -0600

    11:07am

--- Chapter3/src/Assignments/EvenorOdd.java
+++ Chapter3/src/Assignments/EvenorOdd.java
@@ -6,7 +6,18 @@ public class EvenorOdd
 	public static void main(String[] args) 
 	{
 		Scanner input = new Scanner(System.in); 
-
+		
+		System.out.print("Enter an Integer: ");
+		int n1 = input.nextInt();
+		int rem = n1%2;
+		if(rem==1)
+		{
+			System.out.println("The integer "+n1 +" is odd.");
+		}
+		else
+		{	
+			System.out.println("The integer "+n1 +" is even");
+		}
 	}
 
 }
