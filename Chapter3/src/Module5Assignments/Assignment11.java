7b132ed5938e175faf69f64b468fe39d018999d4
46221501 <46221501@L815WD220023.edu.cbe.ab.ca>
2022-04-27 13:52:41 -0600

    1:52pm

--- Chapter3/src/Module5Assignments/Assignment11.java
+++ Chapter3/src/Module5Assignments/Assignment11.java
@@ -11,13 +11,47 @@
 */
 package Module5Assignments;
 
+import java.util.Scanner;
+
 public class Assignment11 
 {
 
 	public static void main(String[] args) 
 	{
+		Scanner input = new Scanner(System.in);
 		
-
+		System.out.println("Enter any Three Digits: ");
+		
+		int numbers = input.nextInt();
+		
+		int firstD = numbers / 10;
+		
+		int secondD = numbers % 10;
+		
+		
+		
+		int max = 0;
+		int min = 0;
+		
+		if(firstD > secondD)//85
+		{
+			max = firstD;
+			min = secondD;
+			
+			
+			
+		}
+		else//58
+		{
+			max = secondD;
+			min = firstD;
+			
+		
+		}	
+		System.out.println("Maximum: "+ max +" Minmum:"+ min );
+		
+		//579
+		//18:43 in bongo video
 	}
 
 }
