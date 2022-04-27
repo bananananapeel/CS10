e55b07794cee52147d20ff3de632b153552a3e42
46221501 <46221501@L815WD220023.edu.cbe.ab.ca>
2022-04-27 13:21:14 -0600

    1:21pm

---
+++ Chapter3/src/Module5Assignments/Assignment11.java
@@ -0,0 +1,27 @@
+/*
+
+	Program: ${Assignment11}.java          Date: ${April 27 2022}
+
+
+	Author: Anjali Sanjay
+	School: CHHS
+	Course: Computer Science 10
+	 
+
+*/
+package Module5Assignments;
+
+public class Assignment11 
+{
+
+	public static void main(String[] args) 
+	{
+		
+
+	}
+
+}
+/*
+screen dump
+
+*/
---
+++ Chapter3/src/Module5Assignments/EvenOrOdd.java
@@ -0,0 +1,40 @@
+/*
+
+	Program: ${EvenOrOdd}.java          Date: ${April 27 2022}
+
+
+	Author: Anjali Sanjay
+	School: CHHS
+	Course: Computer Science 10
+	 
+
+*/
+package Module5Assignments;
+import java.util.Scanner;
+public class EvenOrOdd 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner input = new Scanner(System.in); 
+		
+		System.out.print("Enter an Integer: ");//asks to enter question
+		int n1 = input.nextInt();//user inputs number
+		int rem = n1%2;
+		if(rem==1)//does math to see if number is even or odd
+		{
+			System.out.println("The integer "+n1 +" is odd.");//the number is displayed as odd
+		}
+		else
+		{	
+			System.out.println("The integer "+n1 +" is even");//the number is displayed as even
+		}
+	}
+
+}
+/*
+screen dump
+
+Enter an Integer: 3
+The integer 3 is odd.
+*/
