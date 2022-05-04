85423b2acd20ffa55d93e3476165feef30ab9e60
46221501 <46221501@L815WD220023.edu.cbe.ab.ca>
2022-05-04 13:51:49 -0600

    1:51pm

--- Chapter3/src/Module5Exercises/SwitchStatmentExercise.java
+++ Chapter3/src/Module5Exercises/SwitchStatmentExercise.java
@@ -8,10 +8,26 @@ public class SwitchStatmentExercise
 		int day = 2;
 		switch(day)
 		{
-		case 1;
-		Syste
+			case 1:
+			System.out.println("Saturday");
+			break;
+			case 2:
+			System.out.println("Sunday");
+			break;
 		}
-
+		
+		int dayy = 4;
+		switch(day)
+		{
+			case 1:
+			System.out.println("Saturday");
+			break;
+			case 2:
+			System.out.println("Sunday");
+			break;
+			default:
+			System.out.println("Weekend");
+		}
 	}
 
 }
