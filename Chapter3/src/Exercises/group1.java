bae639cf39d938284c6e8cb9e00711337bb6926c
46221501 <46221501@L815WD220023.edu.cbe.ab.ca>
2022-06-02 14:33:53 -0600

    2:33pm

--- Chapter3/src/Exercises/group1.java
+++ Chapter3/src/Exercises/group1.java
@@ -2,63 +2,12 @@ package Exercises;
 
 import java.util.Scanner;
 
-public class group1 {
+public class group1 
+{
 
 	public static void main(String[] args) 
-	{
-		
-		String one = "play";
-		while (one=="play")
-		{
-		Scanner input = new Scanner(System.in);
-		int comp = 1 + (int)(Math.random()*3);
-		System.out.println("Enter rock paper or scissors: ");
-		String player = input.nextLine();
-		String message = "Error";
-		if (player=="rock" && comp==1)
-		{
-			message = "Computer: rock \r\nIt's a Tie";
-		}
-		else if (player=="paper" && comp==2)
-		{
-			message = "Computer: paper \r\nIt's a Tie";
-		}
-		else if (player=="scissors" && comp==3)
-		{
-			message = "Computer: scissors \r\nIt's a Tie";
-		}
-		else if (player=="rock" && comp==2)
-		{
-			message = "Computer: paper \r\nYou lose!";
-		}
-		else if (player=="paper" && comp==3)
-		{
-			message = "Computer: scissors \r\nYou lose!";
-		}
-		else if (player=="scissors" && comp==1)
-		{
-			message = "Computer: rock \r\nYou lose!";
-		}
-		else if (player=="rock" && comp==3)
-		{
-			message = "Computer: scissors \r\nYou win!";
-		}
-		else if (player=="paper" && comp==1)
-		{
-			message = "Computer: rock \r\nYou win!";
-		}
-		else if (player=="scissors" && comp==2)
-		{
-			message = "Computer: paper \r\nYou win!";
-			
-		}
-		System.out.println("You: "+player+"\r\n"+message+"\r\n\r\n"
-			+ "Enter play to continue playing or anything else to exit the program.");
-		one = input.nextLine();
-		}
-		System.out.println("\r\nYou have exited the program. Have a good day and come back soon!");
+	  {
+	    
+	    
+	  }
 	}
-
-}
-
-
